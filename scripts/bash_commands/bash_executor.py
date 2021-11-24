351a80d0c5a40de7ff9d7d4cb2368aa2f1ece1f1
oddkid80 <jfordoddkid80@gmail.com>
2021-11-24 13:40:52 -0600

    building bash executor

---
+++ scripts/bash_commands/bash_executor.py
@@ -0,0 +1,24 @@
+from subprocess import Popen, PIPE
+from tempfile import NamedTemporaryFile, TemporaryDirectory
+import datetime
+import logging
+
+class bash_executor():
+    def __init__(self,bash_command):
+        self.bash_command = bash_command
+    
+    def execute_bash(self):        
+        tmp_file_prefix = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+        with TemporaryDirectory(prefix='bash_executor_tmp') as tmp_dir:
+            with NamedTemporaryFile(dir=tmp_dir, prefix=tmp_file_prefix) as tmp_file:
+                tmp_file.write(bytes(self.bash_command, 'utf_8'))                
+                tmp_file.flush()
+                tmp_file_name = tmp_file.name
+                
+                logging.info(f'Executing bash command "{self.bash_command}"')
+                sub_proc = Popen(['bash',tmp_file_name],cwd=tmp_dir,stdout=PIPE,stderr=PIPE) 
+                out,err = sub_proc.communicate()                     
+                if err:
+                    logging.error(f'Bash command failed with error {err.decode().strip()}') 
+                    raise Exception(f'Bash command failed with error {err.decode().strip()}') 
+                sub_proc.wait()
