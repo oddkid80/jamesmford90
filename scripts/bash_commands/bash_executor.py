c6f4c885b299514cbf8c38a2e5203ba89712de9d
oddkid80 <jfordoddkid80@gmail.com>
2021-11-29 19:55:25 -0600

    removing __init__ - not required for current context

--- scripts/bash_commands/bash_executor.py
+++ scripts/bash_commands/bash_executor.py
@@ -4,21 +4,20 @@ import datetime
 import logging
 
 class bash_executor():
-    def __init__(self,bash_command):
-        self.bash_command = bash_command
     
-    def execute_bash(self):        
+    def execute_bash(self,bash_command):        
         tmp_file_prefix = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
         with TemporaryDirectory(prefix='bash_executor_tmp') as tmp_dir:
             with NamedTemporaryFile(dir=tmp_dir, prefix=tmp_file_prefix) as tmp_file:
-                tmp_file.write(bytes(self.bash_command, 'utf_8'))                
+                tmp_file.write(bytes(bash_command, 'utf_8'))                
                 tmp_file.flush()
                 tmp_file_name = tmp_file.name
                 
-                logging.info(f'Executing bash command "{self.bash_command}"')
+                logging.info(f'Executing bash command "{bash_command}"')
                 sub_proc = Popen(['bash',tmp_file_name],cwd=tmp_dir,stdout=PIPE,stderr=PIPE) 
                 out,err = sub_proc.communicate()                     
                 if err:
                     logging.error(f'Bash command failed with error {err.decode().strip()}') 
                     raise Exception(f'Bash command failed with error {err.decode().strip()}') 
                 sub_proc.wait()
+        logging.info(f'Sucessfully executed bash command "{bash_command}"')
